3ba020aa3133bccd84f6f95ce9ab57812d47738a
arthurwg <arthurweymar@gmail.com>
2022-05-12 11:25:23 -0300

    Removido os comentários do fonte.

--- EstruturasDeControle.js
+++ EstruturasDeControle.js
@@ -1,15 +1,4 @@
-/*escreval("Digite o nome do aluno:")
- leia(nome)
- escreval("Digite a nota 01 do aluno:")
- leia(nota01)
- escreval("Digite a nota 02 do aluno:")
- leia(nota02)
- media := (nota01 + nota02) / 2
- se media >= 5 entao
-   escreval ("aprovado")
- senao
-   escreval("reprovado")
- fimse*/
+
  var  nota01, nota02, nome;
  nome = prompt ("Digite o nome do aluno:")
  nota01 = prompt ("Digite a nota 01 do aluno:")
